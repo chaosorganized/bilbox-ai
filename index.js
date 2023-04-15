6008a034546828e637a224e9300474df2b78ea5c
arjshiv <arj.shiv@gmail.com>
2023-04-15 03:32:52 +0000

    feat: simple picker

--- index.js
+++ index.js
@@ -14,17 +14,229 @@ app.use(session({
   saveUninitialized: true
 }));
 
+function mapMoodToGenre(mood) {
+  switch (mood) {
+    case 'happy':
+      return 'classical';
+    case 'sad':
+      return 'romantic';
+    case 'energetic':
+      return 'baroque';
+    case 'relaxed':
+      return 'ambient';
+    default:
+      return 'classical';
+  }
+}
 
 app.get('/', async (req, res) => {
-  res.send('Welcome to Bilbox.ai!')
+  const html = `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Homepage</title>
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap');
+
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    body {
+      font-family: 'Roboto', sans-serif;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      background-color: #262626;
+      overflow: hidden;
+    }
+
+    form {
+      position: relative;
+      text-align: center;
+    }
+
+    label {
+      font-size: 1.5rem;
+      color: #f0f0f0;
+    }
+
+    select {
+      font-size: 1.2rem;
+      padding: 0.5rem;
+      margin: 1rem 0;
+      border: none;
+      border-radius: 5px;
+      outline: none;
+      background-color: #f0f0f0;
+      cursor: pointer;
+    }
+
+    button {
+      font-size: 1.2rem;
+      padding: 0.5rem 1.5rem;
+      border: none;
+      border-radius: 5px;
+      background-color: #62c370;
+      color: #f0f0f0;
+      cursor: pointer;
+      transition: background-color 0.3s;
+    }
+
+    button:hover {
+      background-color: #42a250;
+    }
+
+    .circle {
+      position: absolute;
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+      animation: circle-animation 10s linear infinite;
+      background-color: rgba(98, 195, 112, 0.2);
+    }
+
+    .circle:nth-child(1) {
+      top: 20%;
+      left: 20%;
+      animation-delay: 0s;
+    }
+
+    .circle:nth-child(2) {
+      top: 40%;
+      left: 80%;
+      animation-delay: 3s;
+      width: 80px;
+      height: 80px;
+    }
+
+    .circle:nth-child(3) {
+      top: 80%;
+      left: 50%;
+      animation-delay: 5s;
+      width: 120px;
+      height: 120px;
+    }
+
+    .circle:nth-child(4) {
+      top: 60%;
+      left: 10%;
+      animation-delay: 7s;
+      width: 150px;
+      height: 150px;
+    }
+
+    @keyframes circle-animation {
+      0% {
+        transform: translateY(0) scale(1);
+      }
+      50% {
+        transform: translateY(-50px) scale(1.2);
+      }
+      100% {
+        transform: translateY(0) scale(1);
+      }
+    }
+  </style>
+</head>
+<body>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <form action="/stream-v1" method="get">
+    <label for="mood">Select a mood:</label>
+    <br>
+    <select name="mood" id="mood" required>
+      <option value="">--Choose a mood--</option>
+      <option value="happy">Happy</option>
+      <option value="sad">Sad</option>
+      <option value="energetic">Energetic</option>
+      <option value="relaxed">Relaxed</option>
+    </select>
+    <br>
+    <button type="submit">Stream Music</button>
+  </form>
+</body>
+</html>
+  `;
+  res.send(html);
 });
 
-// Google Authorize
+app.get('/stream-v1', async (req, res) => {
+  const mood = req.query.mood;
+  const genre = mapMoodToGenre(mood);
+
+  try {
+    console.info({ mood, genre })
+
+    console.info('picture')
+    // Fetch a picture based on mood
+    const imageString = await createImage({
+    topic: `abstract representation of ${genre} music`
+  });
+  const imageUrl = `data:image/png;base64,${imageString}`;
+
+    console.info('audio')
+  const { audio, name } = await getAudio({
+  genre
+})
+
+    // Create the HTML page with the background image and audio player to stream the music
+    const html = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Streaming Music</title>
+        <style>
+          body {
+            background-image: url('${imageUrl}');
+            background-size: cover;
+            background-position: center;
+            margin: 0;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+          audio {
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+          }
+        </style>
+      </head>
+      <body>
+        <audio controls autoplay loop>
+          <source src="${audio}" type="audio/mpeg">
+          Your browser does not support the audio element.
+        </audio>
+        <script>
+          const audio = document.querySelector('audio');
+          audio.volume = 0.5;
+        </script>
+      </body>
+      </html>
+    `;
+
+    res.send(html);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred while fetching music tracks and background image');
+  }
+});
+
+// Google Authorize - DONE
 app.get('/auth', (req, res) => {
   return res.redirect(authorizationUrl);
 });
 
-// Google OAuth Callback
+// Google OAuth Callback - DONE
 app.get('/oauth2callback', async (req, res) => {
   let responseSent = false;
 
@@ -33,6 +245,7 @@ app.get('/oauth2callback', async (req, res) => {
 
   // Store the tokens securely for later use
   oauth2Client.setCredentials(tokens);
+  req.session.tokens = tokens;
 
   if (!responseSent){
     responseSent = true;
@@ -42,9 +255,12 @@ app.get('/oauth2callback', async (req, res) => {
   }
 });
 
-// Live YouTube Stream
+// Live YouTube Stream  - NA
 app.get('/stream', async(req, res) => {
-   res.send('Stream coming soon!');
+   // res.send('Stream coming soon!');
+  console.log(req.session.tokens)
+
+  
 });
 
 app.get('/image-gen/:topic', async (req, res) => {
--- stream/transcode/transcode.js
+++ stream/transcode/transcode.js
@@ -1,1 +1,103 @@
 // AV Transcoding Coming Soon
+const { google } = require('googleapis');
+const fs = require('fs');
+const { Readable } = require('stream');
+
+async function streamVideo(auth) {
+  const drive = google.drive({ version: 'v3', auth });
+  const youtube = google.youtube({ version: 'v3', auth });
+
+  // Get the Google Drive file ID of the video to be streamed
+  const fileId = 'VIDEO_FILE_ID';
+
+  // Get the video file metadata from Google Drive
+  const { data } = await drive.files.get({ fileId, fields: 'name, size, mimeType' });
+
+  // Check if the file is a valid MP4 video
+  if (data.mimeType !== 'video/mp4') {
+    throw new Error('File is not a valid MP4 video');
+  }
+
+  // Create a readable stream from the Google Drive file
+  const stream = drive.files.get({ fileId, alt: 'media' }, { responseType: 'stream' }).then(response => {
+    return response.data;
+  });
+
+  // Create a new broadcast on YouTube
+  const { data: broadcast } = await youtube.liveBroadcasts.insert({
+    part: ['snippet', 'status'],
+    requestBody: {
+      snippet: {
+        title: 'My Stream',
+        description: 'Description of my stream',
+        scheduledStartTime: new Date().toISOString(),
+      },
+      status: {
+        privacyStatus: 'unlisted', // can be 'public', 'unlisted', or 'private'
+      },
+    },
+  });
+
+  // Create a new stream on YouTube
+  const { data: streamInfo } = await youtube.liveStreams.insert({
+    part: ['snippet', 'cdn'],
+    requestBody: {
+      snippet: {
+        title: 'My Stream',
+      },
+      cdn: {
+        format: '720p',
+        ingestionType: 'rtmp',
+        resolution: 'variable',
+      },
+    },
+  });
+
+  // Bind the YouTube stream to the Google Drive video stream
+  const { data: bindResult } = await youtube.liveBroadcasts.bind({
+    part: ['id,snippet'],
+    requestBody: {
+      id: broadcast.id,
+      streamId: streamInfo.id,
+    },
+  });
+
+  // Get the RTMP ingestion URL and stream key for the YouTube stream
+  const rtmpUrl = streamInfo.cdn.ingestionInfo.ingestionAddress;
+  const streamKey = streamInfo.cdn.ingestionInfo.streamName;
+
+  // Initialize the stream to YouTube
+  const streamOptions = {
+    video: {
+      codec: 'h264',
+      resolution: '720x1280',
+      bitrate: 5000000,
+      fps: 30,
+    },
+    audio: {
+      codec: 'aac',
+      channels: 2,
+      bitrate: 128000,
+    },
+    rtmp: {
+      url: rtmpUrl,
+      key: streamKey,
+    },
+  };
+  const youtubeStream = youtube.liveStreams.list({
+    part: 'id,snippet,cdn',
+    id: streamInfo.id,
+  }, {
+    transformResponse: response => response.data,
+  }).then(response => {
+    const stream = new Readable({
+      read() {},
+    });
+    stream.pipe(fs.createWriteStream('output.flv'));
+    stream.push(response.cdn.ingestionInfo.backupIngestionAddress);
+    return stream;
+  });
+
+  // Stream the video from Google Drive to YouTube
+  stream.pipe(youtubeStream);
+}
